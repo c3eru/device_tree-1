6b57861220ae570181ccac483df3e141088b28a5
Joaquin Guzman <githubjoaquin@gmail.com>
2021-10-15 21:20:19 +0800

    juice: update sepolicy flag according to A12

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,8 +183,9 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
 include device/qcom/sepolicy/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 
 # Treble
 BOARD_VNDK_VERSION := current
