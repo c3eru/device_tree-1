355cab98a44b45b1a5a157bbb7bc2ecb9e010958
mobxCode <yovie079@gmail.com>
2022-01-03 08:29:36 +0700

    juice: disable live wallpaper

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -9,6 +9,9 @@ $(call inherit-product, device/xiaomi/juice/device.mk)
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+# Target
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
