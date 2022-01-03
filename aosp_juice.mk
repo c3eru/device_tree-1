62b910eb813f7e234ac9e3d4d6d73f166debfb8b
mobxCode <yovie079@gmail.com>
2022-01-03 08:34:09 +0700

    juice: resolution bootanimation

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -12,6 +12,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # Target
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_juice
