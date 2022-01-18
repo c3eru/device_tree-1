354a3851a4b41f81218d8e4b0bf0f864d5700be7
c3eru <yyprjkt@gmail.com>
2022-01-18 21:18:35 +0700

    juice: use Proton clang with LLVM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,7 +114,7 @@ BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE) --board ""
 #clang
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
-KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin/aarch64-linux-gnu-
+TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
